1d3546d399e250d92293024b09292dd72e07f60b
Prathamesh <tambe.p@husky.neu.edu>
2019-06-03 19:57:13 -0400

    assignment00.01 pushed

---
+++ webapp/README.md
@@ -0,0 +1,71 @@
+# CSYE - 6225 (User Api)
+==================================
+
+## Getting Started
+
+These instructions will get you a copy of the project up and running on your local machine for development and testing purposes. See deployment for Bookes.
+
+### Prerequisites
+
+To run this you need to have postman,nodejs,mysql and npm installed on your machine. 
+
+### How to use
+
+Git clone this repo on your local machine.
+
+Run following query commands in mysql
+
+create Books database in mysql and create following table in it
+
+```
+-	CREATE TABLE `user` (
+-	`id` INT NOT NULL AUTO_INCREMENT,
+-	`username` VARCHAR(255) NOT NULL,
+-	`password` VARCHAR(255) NOT NULL,
+-	`cdate` TIMESTAMP NOT NULL,
+-	PRIMARY KEY (`id`)
+-	) ENGINE=InnoDB;
+```
+
+and run following commands 
+
+```
+-	cd ../webapp/webapp
+-	npm install
+-	npm start	
+```
+
+and then open http://localhost:8080 on postman to check following scenarios.
+
+1. 	Create new user by accessing http://localhost:8080/user/register with username and password in postman using content type - application/json.
+	-	if user already exists then it will print user already exists.
+	-	else it will create user with generating bcrypt encoded hash.
+
+2.	When user access this http://localhost:8080 url without passing basic authentication it will return error message.
+	-	if user passes valid creds then it will show current date in json response.		
+
+3.	It satisfies all user stories required in assignment00.01.
+
+
+## Built With
+
+* [NodeJs](https://nodejs.org/en/) - Node.js® is a JavaScript runtime built on Chrome's V8 JavaScript engine.
+
+* [MySQL](https://www.mysql.com/) - MySQL is the world's most popular open source database. With its proven performance, reliability and ease-of-use, MySQL has become the leading database choice for web-based applications, used by high profile web properties including Facebook, Twitter, YouTube, Yahoo! and many more.
+
+* [bcrypt](https://www.npmjs.com/package/bcrypt) - A library to help you hash passwords.
+
+## Versioning
+
+We use [github](https://github.com/) for versioning. For the versions available, see the [tags on this repository](https://github.com/prathamesh-tambe/webapp). 
+
+## Group members
+
+**Prahamesh Tambe** - tambe.p@husky.neu.edu
+
+**Ishita Chausalkar** - chausalkar.i@husky.neu.edu
+
+## Acknowledgments
+
+Used this link for boilerplate - [github.com](https://github.com/developit/express-es6-rest-api)
+
---
+++ webapp/src/api/index.js
@@ -0,0 +1,162 @@
+import { version } from '../../package.json';
+import { Router } from 'express';
+import facets from './facets';
+import bcrypt from 'bcrypt';
+
+const saltRounds = 10;
+const myPlaintextPassword = 's0/\/\P4$$w0rD';
+const someOtherPlaintextPassword = 'not_happening';
+
+
+
+export default ({ config, db }) => {
+	let api = Router();
+
+	var checkUser = function(uname){
+		console.log("username",uname)
+		db.query('SELECT * FROM user WHERE username = ?',[uname], function (error, results, fields) {
+  			if (error) {
+				throw error;
+			}else{
+  				//console.log('Data is ', results.length);
+				if(results.length > 0){
+					console.log('Data is true', results.length)
+					return 1;	
+				}else{
+					console.log('Data is false', results.length)
+					return 0;				
+				}
+			}
+		});
+	}
+
+	// mount the facets resource
+	api.use('/facets', facets({ config, db }));
+
+	// perhaps expose some API metadata at the root
+	/*api.get('/', (req, res) => {
+		res.json({ version });
+	});*/
+	
+	api.get('/', function (req, res){
+		if (!req.headers.authorization || req.headers.authorization.indexOf('Basic ') === -1) {
+        		return res.status(401).json({ message: 'User not logged in' });
+    		}else{
+			var header=req.headers['authorization']||'',
+      			token=header.split(/\s+/).pop()||'',
+      			auth=new Buffer.from(token, 'base64').toString(),
+      			parts=auth.split(/:/),
+		      	username=parts[0],
+      			password=parts[1];
+		
+			db.query('SELECT * FROM user WHERE username = ?',[username], function (error, results, fields) {
+  				if (error) {
+					throw error;
+				}else{
+  					if(results.length > 0){
+						bcrypt.compare(myPlaintextPassword, results[0].password, function(err, resv) {
+							console.log("res---------",resv);    				
+							// res == true
+							if (error) throw error;	
+							if(resv){
+								res.json({ crrdate : new Date().toISOString() });
+							}else{
+								res.json({ message : 'password does not match' });
+							}
+						});	
+					}else{
+						console.log('Data is false', results.length)
+						res.json({ message : 'user does not exists' });
+					}
+				}
+			});
+		}		
+	});
+
+
+	api.get('/user/login', (req, res) => {
+		let username = req.body.username;
+		let pass = req.body.password;
+		console.log('req----',req.body.username,req.body.password);
+		
+		db.query('SELECT * FROM user WHERE username = ?',[username], function (error, results, fields) {
+  			if (error) {
+				throw error;
+			}else{
+  				if(results.length > 0){
+					bcrypt.compare(myPlaintextPassword, hash, function(err, res) {
+						console.log("res---------",res);    				
+						// res == true
+					});
+					res.json({ version });
+				}else{
+					res.json({ message:"user not logged in" });
+				}
+			}
+		});
+		
+	});
+	
+	api.post('/user/register',(req,res)=>{
+				
+		let username = req.body.username;
+		let pass = req.body.password;
+		console.log('req----',req.body.username,req.body.password);
+		
+		db.query('SELECT * FROM user WHERE username = ?',[username], function (error, results, fields) {
+  			if (error) {
+				throw error;
+			}else{
+  				//console.log('Data is ', results.length);
+				if(results.length > 0){
+					console.log('Data is true', results.length)
+					res.json({ message:"user already exists" });	
+				}else{
+					console.log('Data is false', results.length)
+					let strongRegex = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})");
+					let status = strongRegex.test(pass);
+					console.log("status",status);
+					if(status){
+						bcrypt.hash(pass, saltRounds, function(err, hash) {
+							//console.log('hash-------',hash);
+							if(err) throw err;							
+							db.query('INSERT INTO user (username, password) VALUES (?, ?); ',[username,hash], function (error, results, fields) {
+  								if (error) throw error;
+  								console.log('The solution is: ', results[0]);
+								res.json({ message:'added successfully' });							
+							});  			
+						});
+					}else{
+						res.json({ message:'password not containg nist stadards' });					
+					}
+										
+				}
+			}
+		});
+
+		//console.log(checkUser(username));
+				
+		//, function(id) {
+
+    		/*	console.log('check user called-------------------',id);    
+
+			let strongRegex = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})");
+			let status = strongRegex.test(pass);
+			console.log("status",status);
+			if(status){
+				bcrypt.hash(pass, saltRounds, function(err, hash) {
+					//console.log('hash-------',hash);
+					/*db.query('insert into ', function (error, results, fields) {
+  						if (error) throw error;
+  						console.log('The solution is: ', results[0].solution);
+					});*/  			
+		/*		});
+			}
+			res.json({  });		
+		*/
+		//});
+		
+				
+	})
+	return api;
+}
