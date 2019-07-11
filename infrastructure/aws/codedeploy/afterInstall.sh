0b01ef14f9994f6c215a2c77bd7eb5f8269f4ffb
Prathamesh <tambe.p@husky.neu.edu>
2019-07-11 18:43:36 -0400

    copy changes 9

--- infrastructure/aws/codedeploy/afterInstall.sh
+++ infrastructure/aws/codedeploy/afterInstall.sh
@@ -22,5 +22,6 @@ cd /home/centos/webapp/
 pwd
 
 sudo npm install
+sudo npm i forever -g
 
-#NODE_ENV=prod NODE_DB_HOST=$rdsEndpoint NODE_S3_BUCKET=$s3bucket NODE_DB_USER=csye6225master NODE_DB_PASS=csye6225password node index.js
+NODE_ENV=prod NODE_DB_HOST=$rdsEndpoint NODE_S3_BUCKET=$s3bucket NODE_DB_USER=csye6225master NODE_DB_PASS=csye6225password forever start index.js
