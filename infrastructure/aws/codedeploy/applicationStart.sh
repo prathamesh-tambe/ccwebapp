f61cff5245ddd8d42343f035d8920b9218f5b558
DD-HAREKAZE <zhao.jiawe@northeastern.edu>
2019-07-25 21:24:03 -0400

    fix appStart.sh

--- infrastructure/aws/codedeploy/applicationStart.sh
+++ infrastructure/aws/codedeploy/applicationStart.sh
@@ -3,7 +3,13 @@
 pwd
 whoami
 cd /home/centos/webapp
-sudo mkdir -p var
+if [ -d "var" ] 
+then
+    echo "Directory /home/centos/webapp/var exists." 
+else
+    sudo mkdir var
+fi
+
 sudo cp /var/.env /home/centos/webapp/var
 sudo chmod 666 .env
 pwd
